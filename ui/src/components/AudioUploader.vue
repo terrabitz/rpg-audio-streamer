654be553d9fdb96cb131fcc82929dbb261ad6106
Trevor Taubitz <terrabitz@protonmail.com>
2025-02-01 15:54:40 -0500

    feat: Add basic file playback
    Signed-off-by: Trevor Taubitz <terrabitz@protonmail.com>

---
+++ ui/src/components/AudioUploader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-file-input label="Select a file" prepend-icon="$music" accept="audio/mp3" @change="onFileChange"></v-file-input>
+    <audio v-if="audioUrl" :src="audioUrl" controls style="padding-top: 15px;"></audio>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const audioUrl = ref<string | null>(null)
+const onFileChange = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  if (target.files && target.files[0]) {
+    const file = target.files[0];
+    audioUrl.value = URL.createObjectURL(file);
+  }
+};
+</script>
--- ui/src/plugins/vuetify.ts
+++ ui/src/plugins/vuetify.ts
@@ -1,6 +1,7 @@
-import { mdiHome } from '@mdi/js'
+import { mdiHome, mdiMusic } from '@mdi/js'
 import { createVuetify } from 'vuetify'
 import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+import 'vuetify/styles'
 
 export default createVuetify({
   icons: {
@@ -8,6 +9,7 @@ export default createVuetify({
     aliases: {
       ...aliases,
       home: mdiHome,
+      music: mdiMusic,
     },
     sets: {
       mdi,
