b9f21a63e03a9baa9d7f386a354f39c6df074b26
Trevor Taubitz <terrabitz@protonmail.com>
2025-02-07 17:31:47 -0500

    patch: Add initial handlers
    Signed-off-by: Trevor Taubitz <terrabitz@protonmail.com>

--- main.go
+++ main.go
@@ -5,11 +5,16 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"io/fs"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"time"
 )
+
+var uploadDir = "./uploads"
 
 //go:embed ui/dist
 var frontend embed.FS
@@ -32,12 +37,79 @@ func run() error {
 	flag.Parse()
 
 	frontendFS := http.FileServer(http.FS(stripped))
-	http.Handle("/", frontendFS)
+
+	mux := http.NewServeMux()
+	mux.Handle("/", frontendFS)
+	mux.HandleFunc("/upload", uploadFile)
+	mux.HandleFunc("/stream/{fileName}", streamFile)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: mux,
+	}
 
 	fmt.Printf("Listening on port %d\n", port)
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); !errors.Is(err, http.ErrServerClosed) {
+	if err := srv.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
 		return fmt.Errorf("error while running server: %w", err)
 	}
 
 	return nil
+}
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	err := r.ParseMultipartForm(10 << 20) // Limit upload size to 10MB
+	if err != nil {
+		http.Error(w, "Failed to parse form: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	file, handler, err := r.FormFile("files")
+	if err != nil {
+		http.Error(w, "Failed to retrieve file: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	dstFile, err := os.Create(filepath.Join(uploadDir, handler.Filename))
+	if err != nil {
+		http.Error(w, "Failed to save file: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer dstFile.Close()
+
+	_, err = io.Copy(dstFile, file)
+	if err != nil {
+		http.Error(w, "Failed to write file: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("File uploaded successfully"))
+}
+
+func streamFile(w http.ResponseWriter, r *http.Request) {
+	fileName := r.PathValue("fileName")
+
+	filePath := filepath.Join(uploadDir, fileName)
+
+	_, err := os.Stat(filePath)
+	if os.IsNotExist(err) {
+		http.Error(w, "File not found", http.StatusNotFound)
+		return
+	}
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		http.Error(w, "Failed to open file: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	w.Header().Set("Content-Type", "audio/mp3")
+	http.ServeContent(w, r, fileName, time.Time{}, file)
 }
