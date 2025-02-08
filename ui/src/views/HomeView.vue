414c175d5b736c05b077bff6218966f71784f661
Trevor Taubitz <terrabitz@protonmail.com>
2025-02-08 15:02:18 -0500

    fix: Add conditional button for authenticated users in HomeView
    Signed-off-by: Trevor Taubitz <terrabitz@protonmail.com>

--- ui/src/views/HomeView.vue
+++ ui/src/views/HomeView.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+import { useAuthStore } from '../stores/auth';
+
+const auth = useAuthStore()
+</script>
+
 <template>
   <v-container>
     <h1>Welcome to Skald Bot!</h1>
+    <v-btn v-if="auth.authenticated" to="table">My Table</v-btn>
   </v-container>
 </template>
