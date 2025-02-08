62fcba4ea4b386645adec07b16b0096bedfffe30
Trevor Taubitz <terrabitz@protonmail.com>
2025-02-07 23:54:55 -0500

    feat: Implement WebSocket support for real-time communication
    Signed-off-by: Trevor Taubitz <terrabitz@protonmail.com>

---
+++ internal/websocket/hub.go
@@ -0,0 +1,58 @@
+package websocket
+
+import (
+	"log/slog"
+	"sync"
+)
+
+type Hub struct {
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+	mu         sync.RWMutex
+	logger     *slog.Logger
+}
+
+func NewHub(logger *slog.Logger) *Hub {
+	return &Hub{
+		clients:    make(map[*Client]bool),
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		logger:     logger,
+	}
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.mu.Lock()
+			h.clients[client] = true
+			h.mu.Unlock()
+		case client := <-h.unregister:
+			h.mu.Lock()
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+			h.mu.Unlock()
+		case message := <-h.broadcast:
+			h.mu.RLock()
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+			h.mu.RUnlock()
+		}
+	}
+}
+
+func (h *Hub) Register(c *Client) {
+	h.register <- c
+}
