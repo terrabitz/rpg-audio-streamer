3cae47c61d6df3cc6138a92c94fb9a4392866cfc
Trevor Taubitz <terrabitz@protonmail.com>
2025-02-16 23:00:12 -0500

    feat: Enhance volume mixer layout with labeled sliders for improved usability in PlayerView
    Signed-off-by: Trevor Taubitz <terrabitz@protonmail.com>

--- ui/src/views/PlayerView.vue
+++ ui/src/views/PlayerView.vue
@@ -73,10 +73,17 @@ watch(masterVolume, (newVolume) => {
     <v-card class="mt-4 audio-slider-card" border="sm" density="compact">
       <v-card-title>Volume Mixer</v-card-title>
       <v-card-text>
-        <VolumeSlider v-model="masterVolume" class="mb-4" />
-        <v-divider class="mb-4" />
-        <VolumeSlider v-for="type in trackTypeStore.trackTypes" :key="type.id"
-          v-model="audioStore.typeVolumes[type.name]" :color="type.color" class="mb-4" />
+        <div class="mixer-controls">
+          <div class="mixer-row">
+            <span class="mixer-label">Master</span>
+            <VolumeSlider v-model="masterVolume" class="mixer-slider" />
+          </div>
+          <v-divider class="my-4" />
+          <div v-for="type in trackTypeStore.trackTypes" :key="type.id" class="mixer-row">
+            <span class="mixer-label">{{ type.name }}</span>
+            <VolumeSlider v-model="audioStore.typeVolumes[type.name]" :color="type.color" class="mixer-slider" />
+          </div>
+        </div>
       </v-card-text>
     </v-card>
 
@@ -97,5 +104,24 @@ watch(masterVolume, (newVolume) => {
 
 .audio-slider {
   width: 100%;
+}
+
+.mixer-controls {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+}
+
+.mixer-row {
+  display: flex;
+  align-items: center;
+  gap: 16px;
+}
+
+.mixer-label {
+  width: 100px;
+  flex-shrink: 0;
+  font-size: 1.0rem;
+  font-weight: 600;
 }
 </style>
