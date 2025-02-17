90f228bcfa3733fdc16fb4a437edbface2dc7584
Trevor Taubitz <terrabitz@protonmail.com>
2025-02-16 22:47:18 -0500

    feat: Add divider between volume sliders in PlayerView for improved UI clarity
    Signed-off-by: Trevor Taubitz <terrabitz@protonmail.com>

--- ui/src/views/PlayerView.vue
+++ ui/src/views/PlayerView.vue
@@ -74,6 +74,7 @@ watch(masterVolume, (newVolume) => {
       <v-card-title>Volume Mixer</v-card-title>
       <v-card-text>
         <VolumeSlider v-model="masterVolume" label="Master" class="mb-4" />
+        <v-divider class="mb-4" />
         <VolumeSlider v-for="type in trackTypeStore.trackTypes" :key="type.id"
           v-model="audioStore.typeVolumes[type.name]" :label="type.name" :color="type.color" class="mb-4" />
       </v-card-text>
