77d6831363d58501cee0983018ef820d4f0ae65d
Trevor Taubitz <trevor.taubitz@flocksafety.com>
2025-06-10 22:16:57 -0400

    fix: Remove duplicated code from TableView
    Signed-off-by: Trevor Taubitz <trevor.taubitz@flocksafety.com>

--- ui/src/views/TableView.vue
+++ ui/src/views/TableView.vue
@@ -1,56 +1,22 @@
 <script setup lang="ts">
-import { useAudioStore, type AudioTrack } from '@/stores/audio'
-import { useWebSocketStore } from '@/stores/websocket'
-import { computed, onMounted, onUnmounted, ref } from 'vue'
+import { computed, onMounted } from 'vue'
 import TableViewGM from './TableViewGM.vue'
 import TableViewPlayer from './TableViewPlayer.vue'
-import { useAppBar } from '../composables/useAppBar'
 import { useAuthStore } from '../stores/auth'
-import { useJoinStore } from '../stores/join'
 
 const auth = useAuthStore()
-const wsStore = useWebSocketStore()
-const audioStore = useAudioStore()
-const { setTitle, setActions } = useAppBar()
 
 const isPlayerView = computed(() => {
-  return auth.role === 'player' && auth.authenticated
+  return auth.role === 'player' || !auth.authenticated
 })
 
 const isGMView = computed(() => {
   return auth.role === 'gm' && auth.authenticated
 })
 
-function handleSyncAll(message: { method: string, payload: { tracks: AudioTrack[] } }) {
-  if (message.method === 'syncAll' && message.payload.tracks) {
-    console.log('handleSyncAll', message)
-    audioStore.syncTracks(message.payload.tracks)
-  }
-}
-
-function handleSyncTrack(message: { method: string, payload: Partial<AudioTrack> }) {
-  if (message.method === 'syncTrack' && message.payload.fileID) {
-    console.log('handleSyncTrack', message)
-    const { fileID, ...updates } = message.payload
-    audioStore.updateTrackState(fileID, updates)
-  }
-}
 
 onMounted(async () => {
   await auth.checkAuthStatus()
-
-  // Add WebSocket message handlers
-  wsStore.addMessageHandler(handleSyncAll)
-  wsStore.addMessageHandler(handleSyncTrack)
-})
-
-onUnmounted(() => {
-  setActions([])
-  setTitle('RPG Audio Streamer')
-
-  // Remove WebSocket handlers
-  wsStore.removeMessageHandler(handleSyncAll)
-  wsStore.removeMessageHandler(handleSyncTrack)
 })
 </script>
 
