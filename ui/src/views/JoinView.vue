fed57b909b63df26b65ed9b4ee1b8dac0317c0c5
Trevor Taubitz <trevor.taubitz@flocksafety.com>
2025-12-01 22:42:17 -0500

    fix: Cleanup wording on join page
    Signed-off-by: Trevor Taubitz <trevor.taubitz@flocksafety.com>

--- ui/src/views/JoinView.vue
+++ ui/src/views/JoinView.vue
@@ -7,12 +7,11 @@ const { getBaseUrl } = useBaseUrl()
 <template>
   <v-container>
     <div class="text-center my-8">
-      <h2 class="text-h4 mb-4">Join a Game Session</h2>
-      <p class="mb-6">You need a table link from your Game Master to connect to the audio stream.</p>
+      <h2 class="text-h4 mb-4">How to Join</h2>
       <v-card>
         <v-card-text>
-          <p>Ask your GM to share their table link with you</p>
-          <p class="mt-4">The link will look like
+          <p>Ask your GM to share their invite link with you</p>
+          <p class="mt-4">The link will look like this:
             <code class="px-2 py-1 rounded bg-primary-lighten-5 text-secondary">
                 {{ getBaseUrl() }}/table/abc123...
               </code>
